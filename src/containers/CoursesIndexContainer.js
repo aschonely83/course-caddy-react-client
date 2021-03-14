4576e965d1f8a8ce6a9a50baf050d92f344257d1
aschonely83 <aschonely@gmail.com>
2021-03-14 18:10:50 -0400

    set up get request for courses

--- src/containers/CoursesIndexContainer.js
+++ src/containers/CoursesIndexContainer.js
@@ -1,10 +1,37 @@
 import React, { Component} from 'react'
+import CoursesList from '../components/CoursesList';
+
 
 export default class CoursesIndexContainer extends Component {
-  render() {
+  
+    state = {
+      courses: [],
+      loading: true    
+    }
+
+    componentDidMount() {
+      fetch('http://localhost:3001/courses', {
+          method: 'get',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(res => res.json())
+          .then(coursesJson => {
+            console.log('courses', coursesJson)
+            this.setState({
+              courses: coursesJson,
+              loading: false
+            })
+          })
+        
+      }
+    
+    render() {
     return (
-      <section className="max-w-5xl mx-auto mt-16">
-          Courses go here
+      <section className="max-w-5xl w-11/12 mx-auto mt-16">
+          {this.state.loading ? 'loading' : <CoursesList courses={this.state.courses} /> }
       </section>  
     )    
   }    
