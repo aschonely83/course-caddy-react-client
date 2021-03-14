bb84a96f8b109005b3a183afbc070cb85e49ec27
aschonely83 <aschonely@gmail.com>
2021-03-14 16:08:18 -0400

    set up course containers

---
+++ src/containers/CoursesIndexContainer.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react'
+
+export default class CoursesIndexContainer extends Component {
+  render() {
+    return (
+      <section className="max-w-5xl mx-auto mt-16">
+          Courses go here
+      </section>  
+    )    
+  }    
+}
