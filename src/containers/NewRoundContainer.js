997facfef6f170655eed775d6cc88f89cfa7bf50
aschonely83 <aschonely@gmail.com>
2021-03-15 20:05:43 -0400

    built handle submit for new round form

--- src/containers/NewRoundContainer.js
+++ src/containers/NewRoundContainer.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
 export default class NewRoundContainer extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const body = new FormData();
+    
+    body.append("round[par]", form.par.value);
+    body.append("round[score]", form.score.value);
+    body.append("round[course_id]", this.props.match.params.course_id);
+
+    fetch("http://localhost:3001/rounds", {
+      method: "post",
+      body    
+    })
+      .then(res => res.json())
+      .then(roundJson => {
+        this.props.history.push(`/courses/${this.props.match.params.courseId}`);  
+      })
+  }
+
   render() {
     return ( 
     <form
-      className="max-w-4xl w-11/12 mx-auto mt-16 shadow-lg px-8 py-6">
+      className="max-w-4xl w-11/12 mx-auto mt-16 shadow-lg px-8 py-6"
+      onSubmit={this.handleSubmit}
+    >      
     <h1 className="text-center text-3xl font-semibold mb-2">New Round</h1>
     <fieldset className="">
       <input
