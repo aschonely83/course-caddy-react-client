99a84082f0cc17f65cb98effbec53833047167cb
aschonely83 <aschonely@gmail.com>
2021-03-22 17:38:13 -0400

    working form and post request for rounds

--- src/containers/NewRoundContainer.js
+++ src/containers/NewRoundContainer.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react'
 
 export default class NewRoundContainer extends Component {
-  state= {
-  par: '',
-  score: ''
-  }
+  //state = {
+  //par: '',
+  //score: ''
+  //}
   
-  handleChange = (e) => {
-    this.setState({
-    [e.target.par]: e.target.value,
-    [e.target.score]: e.target.value  
-    })  
-  }
+ // handleChange = (e) => {
+ //   this.setState({
+ //   [e.target.par]: e.target.value,
+ //   [e.target.score]: e.target.value  
+ //   })  
+ // }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    const form = e.target
+    const body = new FormData()
+    
+    body.append('round[par]', form.par.value)
+    body.append('round[score]', form.score.value)
+    body.append('round[course_id]', this.props.match.params.courseId)
+
     fetch("http://localhost:3001/rounds", {
-      method: 'POST',
-      headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({round:this.state}),
+      method: 'post',
+      body,
     })
       .then(res => res.json())
       .then((roundJson) => {
-        this.props.history.push(`/courses/${this.props.match.params.courseId}`);
+        console.log(roundJson)
+       //this.props.history.push(`/courses/${this.props.match.params.courseId}`);
       })
   }
 
   render() {
     return ( 
     <form
-       onSubmit={this.handleSubmit}
       className="max-w-4xl w-11/12 mx-auto mt-16 shadow-lg px-8 py-6"
+      onSubmit={this.handleSubmit}
     >      
     <h1 className="text-center text-3xl font-semibold mb-8 ">New Round</h1>
     <fieldset className="">
@@ -41,18 +45,14 @@ export default class NewRoundContainer extends Component {
       <input
         type="text"
         name="par"
-        onChange={this.handleChange}
-        value={this.state.par}
         className="w-full border-2 p-4 my-4" 
        />
     </fieldset>
-    <fieldset class>
+    <fieldset className="">
       <label className="block uppercase">Score</label>
       <input
         type="text"
         name="score"
-        onChange={this.handleChange}
-        value={this.state.score}
         className="w-full border-2 p-4 my-4" 
        />
     </fieldset>
