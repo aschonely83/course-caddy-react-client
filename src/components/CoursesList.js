c72442b7a290579ba194f3ecc9d903e0ddc4a7ac
aschonely83 <aschonely@gmail.com>
2021-03-30 19:51:56 -0400

    fixed typo

--- src/components/CoursesList.js
+++ src/components/CoursesList.js
@@ -4,7 +4,7 @@ import CourseListItem from './CourseListItem';
 const CoursesList = ({ courses }) => {
   return (
     <div>
-      <h1 className="pb-6">CoursesList</h1>      
+      <h1 className="pb-6">Course List</h1>      
       <ul>
         {courses.map((course) => ( 
           <CourseListItem key={course.id} course={course} />
